b78d2b3820383ef536d0862006b0622109f00119
motoomisomehara <motin.ba22@gmail.com>
2017-06-29 13:50:25 +0000

    CRUD messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,6 +18,14 @@ class MessagesController < ApplicationController
   end
 
   def update
+     @message = Message.find(params[:id])
+
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
   end
 
   def destroy
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,3 @@
 class Message < ApplicationRecord
 end
+message.content = 'hello'
